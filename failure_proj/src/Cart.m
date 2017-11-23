db21e5060aaffe36a0f0fb1ef222a1f3601f40eb
Edoardo Ghini <dinies@Edoardos-MacBook-Pro.local>
2017-11-23 19:58:42 +0100

    refactoring  to build a fully ObjectOriented Logic

--- failure_proj/src/Cart.m
+++ failure_proj/src/Cart.m
@@ -7,29 +7,25 @@ classdef Cart < handle
       dx = 0;
       ddx =0;
       t = 0;
-      
+      vertices= zeros(4,2);
+      lines_drawn= zeros(4);
    end
    methods
-       
-       
       function self = Cart(mass, dump)
          self.M= mass;
          self.D= dump;
-         
+         compute_vertices(self);
       end
 
 
 
       function [curr_x, curr_dx, curr_ddx, new_t ]  = transition_model(self, u, delta_t)
 
-         %dyn model; for now sinusoidal input (change that)
          new_t= self.t+delta_t;
          dyn_model = @(t) (u(t) - self.D* self.dx) / self.M ;
          curr_ddx= dyn_model(self.t);
-         
          sol_dx = ode45( @(t, unused) dyn_model(t) , [self.t new_t] , self.dx);
          curr_dx = deval(sol_dx, new_t);
-         
          sol_x = ode45( @(t,unused) curr_dx, [self.t new_t], self.x );
          curr_x= deval(sol_x, new_t);
 
@@ -42,6 +38,54 @@ classdef Cart < handle
          self.t= new_t;
 
       end
-      
+
+      function open_loop_sym(self, u, tot_t , delta_t)
+        step_num= tot_t / delta_t;
+        data = zeros( step_num, 4);
+
+        figure(1),hold on;
+        axis([-10 250 0 180]);
+        title( 'world representation'), xlabel('x'), ylabel('z')
+        draw(self);
+        for i= 1:step_num
+	        data(i,:)= [self.t, self.x, self.dx, self.ddx];
+	        [x, dx, ddx, t] = transition_model(self, u, delta_t);
+	        update_model( self, [x, dx, ddx] , t);
+          del_lines_drawn(self);
+          compute_vertices(self);
+          draw(self);
+          pause(0.01);
+        end
+        figure(2)
+        plot(data(:,1),data(:,2),'-x',data(:,1),data(:,3),'-o', data(:,1),data(:,4),'-.' );
+
+      end
+
+      function draw(self)
+       self.lines_drawn(1)= get_line_between_vert(self,1,2);
+       self.lines_drawn(2)= get_line_between_vert(self,2,3);
+       self.lines_drawn(3)= get_line_between_vert(self,3,4);
+       self.lines_drawn(4)= get_line_between_vert(self,4,1);
+      end
+      function compute_vertices(self)
+
+	      self.vertices(1,:)= [self.x-5,0];
+	      self.vertices(2,:)= [self.x-5,7];
+	      self.vertices(3,:)= [self.x+5,7];
+	      self.vertices(4,:)= [self.x+5,0];
+
+      end
+      function l= get_line_between_vert(self, first, second)
+        l= line( [ self.vertices(first,1), self.vertices(second,1)],[self.vertices(first,2), self.vertices(second,2)],'LineWidth', 2);
+      end
+      function del_lines_drawn(self)
+        for i= 1:4
+          delete(self.lines_drawn(i));
+        end
+      end
+
+
+
+
    end
 end
--- failure_proj/src/main.m
+++ failure_proj/src/main.m
@@ -2,36 +2,53 @@ close all
 clear
 clc
 
-delta_t= 0.01;
+tot_sim_t= 10;
+delta_t= 0.1;
 obj = Cart( 1 , 0.3 );
-%sinusoidal input
-%u = @(t) sin(10*t);
+%sinusoidal input it doesn't work pproperly because the dynamic
+%model is not ready to process a negative u
+%u = @(t) 2000*sin(10*t);
 
 %constant input
 u = @(t) 6+( t * 0); %simplify to 3 and it is compatible with the trasition_model call
 
-num_of_steps= 1/delta_t;
 
 
-old_states= zeros(num_of_steps, 4);
-figure(1),hold on;
-axis([-4 8 0 2]);
-title( 'world representation'), xlabel('x'), ylabel('z')
-[ l1 ,l2 ,l3, l4] = drawCart( obj.x);
-old_draw= [ l1 ,l2, l3, l4];
 
-for i= 1:num_of_steps
-	pause(0.01) 
-	old_states(i,:)= [obj.t, obj.x, obj.dx, obj.ddx];
 
-	[ l1 ,l2 ,l3, l4] = drawCart( obj.x, old_draw);
-	old_draw= [ l1 ,l2, l3, l4];
+open_loop_sym( obj, u, 15, 0.1);
 
-	[x, dx, ddx, t] = transition_model(obj, u, delta_t);
-	update_model( obj, [x, dx, ddx] , t);
-end
 
-figure(2)
-plot(old_states(:,1),old_states(:,2),'-o',old_states(:,1),old_states(:,3),'-.', old_states(:,1),old_states(:,4),'-x' );
+
+
+
+
+%incremental quadratic input
+%u= @(t) 3*t.^2;
+
+                                %logaritmic input
+%u= @(t)  7*log(t); BUGGED
+
+%num_of_steps= tot_sim_t/delta_t;
+%data= zeros(num_of_steps, 4);
+%figure(1),hold on;
+%axis([-4 300 0 10]);
+%title( 'world representation'), xlabel('x'), ylabel('z')
+%[ l1 ,l2 ,l3, l4] = drawCart( obj.x);
+%old_draw= [ l1 ,l2, l3, l4];
+
+%for i= 1:num_of_steps
+%	pause(0.01);
+%	data(i,:)= [obj.t, obj.x, obj.dx, obj.ddx];
+
+%	[ l1 ,l2 ,l3, l4] = drawCart( obj.x, old_draw);
+%	old_draw= [ l1 ,l2, l3, l4];
+
+%	[x, dx, ddx, t] = transition_model(obj, u, delta_t);
+%	update_model( obj, [x, dx, ddx] , t);
+%end
+
+%figure(2)
+%plot(data(:,1),data(:,2),'-o',data(:,1),data(:,3),'-.', data(:,1),data(:,4),'-x' );
 
 
