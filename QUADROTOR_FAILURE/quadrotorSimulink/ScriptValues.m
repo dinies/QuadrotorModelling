1f17cbccab23e563d5134bb9c38c3fd44fb492b7
dinies <edoardo.ghini@live.it>
2018-05-07 20:08:11 +0200

    added the vrep integration, buggy

--- QUADROTOR_FAILURE/quadrotorSimulink/ScriptValues.m
+++ QUADROTOR_FAILURE/quadrotorSimulink/ScriptValues.m
@@ -50,3 +50,26 @@ z_constr = [z0, zd, 0, 0]';
 x_coeff = A^-1*x_constr;
 y_coeff = A^-1*y_constr;
 z_coeff = A^-1*z_constr;
+
+%% vrep connection-related initializations
+vrep=remApi('remoteApi'); % using the prototype file (remoteApiProto.m)
+vrep.simxFinish(-1); % just in case, close all opened connections
+clientID=vrep.simxStart('127.0.0.1',19997,true,true,5000,5);
+%check for connection
+if (clientID>-1)
+    disp('Connected to remote API server');
+    
+    vrep.simxSynchronous(clientID,true);
+    vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot);
+    
+    [~,quadBase]=vrep.simxGetObjectHandle(clientID,'Quadricopter_base',vrep.simx_opmode_blocking);
+    [~,floor]=vrep.simxGetObjectHandle(clientID,'ResizableFloor_5_25',vrep.simx_opmode_blocking);
+
+    [~,~]=vrep.simxGetObjectPosition(clientID,quadBase,floor,vrep.simx_opmode_streaming);
+    [~,~]=vrep.simxGetObjectOrientation(clientID,quadBase,floor,vrep.simx_opmode_streaming);
+    [~,~,~]=vrep.simxGetObjectVelocity(clientID,quadBase,vrep.simx_opmode_streaming);
+    
+    
+    open_system('quadSimulink');
+    sim('quadSimulink');
+end
