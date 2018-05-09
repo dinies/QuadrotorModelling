6e97294504b90057de5e0fd561dc4427a69c819c
dinies <edoardo.ghini@live.it>
2018-05-09 18:24:09 +0200

    modified simulink scheme: commented out trajectory planning block, now we use only error on position for outer, added two pid controllers before outer and inner, switched back to nominal model, vrep connection is there but it is commented out

--- QUADROTOR_FAILURE/quadrotorSimulink/ScriptValues.m
+++ QUADROTOR_FAILURE/quadrotorSimulink/ScriptValues.m
@@ -1,77 +1,71 @@
 clc
 clear
-% Constant values %
-
-% g = 9.81;
-% m = 0.5;
-% l = 0.255;
-% d = 2.4*10^(-3);
-% 
-% Ixx = 5.9*10^(-3);
-% Iyy = 5.9*10^(-3);
-% Izz = 1.16*10^(-3);
-
+% Constant values 
 m= 0.5;
 g= 9.81;
-d= 1/2; %2.4*10^-3;
-kr = 1/2; %2*10^-3; %extimate for r = 6 (1 rounds /second) 
-l= 0.255;
+d= 2.4*10^-3;
+l= 0.255;% 0.127;vrep measurement
+kr = 1/2;%2*10^-3; %extimate for r = 6 (1 rounds /second) 
+Ixx= 5.9*10-3;
+Iyy= 5.9*10-3;
+Izz= 1.16*10-3;
 
 
-xd = 0;
-yd = 0;
-zd = 5;
-% Initial state %
+% Desired state
+x_d = 5;
+y_d = 5;
+z_d = 5;
 
-x0 = 0;
-y0 = 0;
-z0 = 5;
+% Initial state
+x0 = 5;
+y0 = 5;
+z0 = 50;
 dx0 = 0;
 dy0 = 0;
 dz0 = 0;
-p0 = 0; %-0.5;
-q0 = 0; %0.5;
+p0 = 0.03; %-0.5;
+q0 = 0.02; %0.5;
 r0 = 2.7;
-phi0 = 0;
-theta0 = 0;
-psi0 = pi/4;
+phi0 = 0.075;
+theta0 = 0.025;
+psi0 = 0.1;
 
 %% Compute cubic trajectory
 
-t = 10;
-
-A = [0, 0, 0, 1;
-    t^3, t^2, t, 1;
-    0, 0, 1, 0;
-    3*t^2, 2*t, 1, 0];
-
-x_constr = [x0, xd, 0, 0]';
-y_constr = [y0, yd, 0, 0]';
-z_constr = [z0, zd, 0, 0]';
-
-x_coeff = A^-1*x_constr;
-y_coeff = A^-1*y_constr;
-z_coeff = A^-1*z_constr;
+% t = 10;
+% 
+% A = [0, 0, 0, 1;
+%     t^3, t^2, t, 1;
+%     0, 0, 1, 0;
+%     3*t^2, 2*t, 1, 0];
+% 
+% x_constr = [x0, xd, 0, 0]';
+% y_constr = [y0, yd, 0, 0]';
+% z_constr = [z0, zd, 0, 0]';
+% 
+% x_coeff = A^-1*x_constr;
+% y_coeff = A^-1*y_constr;
+% z_coeff = A^-1*z_constr;
 
 %% vrep connection-related initializations
-vrep=remApi('remoteApi'); % using the prototype file (remoteApiProto.m)
-vrep.simxFinish(-1); % just in case, close all opened connections
-clientID=vrep.simxStart('127.0.0.1',19997,true,true,5000,5);
-%check for connection
-if (clientID>-1)
-    disp('Connected to remote API server');
-    
-    vrep.simxSynchronous(clientID,true);
-    vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot);
-    
-    [~,quadBase]=vrep.simxGetObjectHandle(clientID,'Quadricopter_base',vrep.simx_opmode_blocking);
-    [~,floor]=vrep.simxGetObjectHandle(clientID,'ResizableFloor_5_25',vrep.simx_opmode_blocking);
-
-    [~,~]=vrep.simxGetObjectPosition(clientID,quadBase,floor,vrep.simx_opmode_streaming);
-    [~,~]=vrep.simxGetObjectOrientation(clientID,quadBase,floor,vrep.simx_opmode_streaming);
-    [~,~,~]=vrep.simxGetObjectVelocity(clientID,quadBase,vrep.simx_opmode_streaming);
-    
-    
+% vrep=remApi('remoteApi'); % using the prototype file (remoteApiProto.m)
+% vrep.simxFinish(-1); % just in case, close all opened connections
+% clientID=vrep.simxStart('127.0.0.1',19997,true,true,5000,5);
+% %check for connection
+% if (clientID>-1)
+%     disp('Connected to remote API server');
+%     
+%     vrep.simxSynchronous(clientID,true);
+%     vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot);
+%     
+%     [~,quadBase]=vrep.simxGetObjectHandle(clientID,'Quadricopter_base',vrep.simx_opmode_blocking);
+%     [~,floor]=vrep.simxGetObjectHandle(clientID,'ResizableFloor_5_25',vrep.simx_opmode_blocking);
+% 
+%     [~,~]=vrep.simxGetObjectPosition(clientID,quadBase,floor,vrep.simx_opmode_streaming);
+%     [~,~]=vrep.simxGetObjectOrientation(clientID,quadBase,floor,vrep.simx_opmode_streaming);
+%     [~,~,~]=vrep.simxGetObjectVelocity(clientID,quadBase,vrep.simx_opmode_streaming);
+%     
+%     
     open_system('quadSimulink');
     sim('quadSimulink');
-end
+% end
