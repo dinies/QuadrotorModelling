432239d34e064db1f4180cc21e68083146b216a4
Giuseppe L'Erario <gl.giuseppelerario@gmail.com>
2018-07-06 11:35:05 +0200

    Report template for haptic interfaces.

---
+++ AUVSI_SUAS/obstacleAvoidance/MotionPlanner.m
@@ -0,0 +1,126 @@
+classdef MotionPlanner < handle
+  properties
+    gamma
+    attrForce
+    env
+    Ka
+    Kb
+    rho
+    wallInfluenceRange
+    Kwall
+    coordsWallPotentials
+  end
+
+  methods
+    function self = MotionPlanner(env)
+      self.gamma= 2;
+      self.env=env;
+      self.Ka = 2.4;
+      self.Kb = 4.0;
+      self.rho= env.unitaryDim*3;
+      self.wallInfluenceRange = env.unitaryDim*4;
+      self.Kwall = 1.5;
+    end
+
+    function f = computeArtificialForce( self)
+      attr = computeAttrForce(self);
+      rep = computeRepForce(self,self.env.obstacles);
+      f = attr + rep;
+    end
+
+    function f = computeAttrForce(self )
+      err= [ self.env.goal.coords.x - self.env.agent.coords.x ;
+             self.env.goal.coords.y - self.env.agent.coords.y
+           ];
+
+      normErr= sqrt( err(1,1)^2 + err(2,1)^2 );
+      if  normErr <= self.rho
+        f = self.Ka * err ;
+      else
+        f = self.Kb * err / normErr;
+      end
+    end
+
+    function f= computeRepForce(self, obs)
+      f = zeros(2,1);
+      for i = 1:size(obs,1)
+        obstacle = obs(i);
+        dist =sqrt( (obstacle.coords.x - self.env.agent.coords.x)^2 + (obstacle.coords.y - self.env.agent.coords.y)^2 );
+
+        clearance = dist - ( self.env.agent.radius + obstacle.radius);
+
+        if clearance <= obstacle.influenceRange
+          vec=[  self.env.agent.coords.x - obstacle.coords.x ;
+                 self.env.agent.coords.y - obstacle.coords.y
+              ];
+
+          gradientClearance =  (  1/sqrt((obstacle.coords.x - self.env.agent.coords.x)^2 +(obstacle.coords.y - self.env.agent.coords.y)^2) )*vec;
+          force = (obstacle.Kr/clearance^2) * ( 1/clearance - 1/obstacle.influenceRange)^(self.gamma-1) * gradientClearance;
+        else
+          force = zeros(2,1);
+        end
+        f = f + force;
+      end
+
+      forceFromWalls = computeRepWalls( self);
+      f = f + forceFromWalls;
+    end
+
+    function f = computeRepWalls(self)
+
+      f = zeros(2,1);
+      self.coordsWallPotentials= [
+               self.env.agent.coords.x, 0;
+               self.env.length(1,1), self.env.agent.coords.y;
+               self.env.agent.coords.x, self.env.length(2,1);
+               0, self.env.agent.coords.y
+      ];
+
+      coordsWalls = self.coordsWallPotentials;
+      for i= 1:size(coordsWalls,1)
+
+        dist =sqrt( (coordsWalls(i,1) - self.env.agent.coords.x)^2 + (coordsWalls(i,2) - self.env.agent.coords.y)^2 );
+        clearance = dist - ( self.env.agent.radius);
+        if clearance <= self.wallInfluenceRange
+          vec=[  self.env.agent.coords.x - coordsWalls(i,1);
+                 self.env.agent.coords.y - coordsWalls(i,2)
+              ];
+          gradientClearance =  (  1/sqrt((coordsWalls(i,1) - self.env.agent.coords.x)^2 +(coordsWalls(i,2) - self.env.agent.coords.y)^2) )*vec;
+          force = (self.Kwall/clearance^2) * ( 1/clearance - 1/self.wallInfluenceRange)^(self.gamma-1) * gradientClearance;
+        else
+          force = zeros(2,1);
+        end
+        f = f + force;
+
+      end
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ AUVSI_SUAS/obstacleAvoidance/ObstacleCreator.m
@@ -0,0 +1,145 @@
+classdef ObstacleCreator  < handle
+  properties
+    obstacles
+    color
+    Kr
+  end
+
+  methods
+    function self= ObstacleCreator(env, arg ,currObs, Kr)
+
+      if nargin > 2
+        self.obstacles= currObs;
+      else
+        self.obstacles= [] ;
+      end
+      if nargin > 3
+        self.Kr = Kr;
+      else
+        self.Kr = 10;
+      end
+      self.color = [0.3, 0.84, 0.8 ];
+      if isscalar(arg)
+        generateRandomObs(self, env, arg);
+      else
+        generateObsFromMat(self, env, arg);
+      end
+    end
+
+    function coords = createRandCoord(~,env)
+      length = env.length;
+      offset= env.unitaryDim;
+      extension_x= length(1,1) - 2*offset;
+      extension_y= length(2,1) - 2*offset;
+
+      coords.x = offset + rand()*extension_x;
+      coords.y = offset + rand()*extension_y;
+      coords.z = 0;
+    end
+
+
+
+    function r=  createRandRadius(~,env)
+      %avgWidth= env.unitaryDim*2;
+      %variance = avgWidth/6;
+
+      avgWidth = 50; % auv-si specific values
+      variance = 43;
+
+      if rand() >= 0.5
+        willBePos= 1;
+      else
+        willBePos= 0;
+      end
+      offset= rand()*variance;
+      if willBePos
+        r=  avgWidth+offset;
+      else
+        r= avgWidth-offset;
+      end
+    end
+
+    function insertObstacle( self, coords,radius, env)
+      epsilon = env.unitaryDim/1000;
+      obs = Obstacle(coords.x, coords.y, coords.z,  radius, self.color, self.Kr);
+      distFromGoal= sqrt( (obs.coords.x - env.goal.coords.x)^2 + (obs.coords.y - env.goal.coords.y)^2);
+      if distFromGoal <= (env.agent.radius + obs.influenceRange + obs.radius)
+        obs.influenceRange= distFromGoal - (env.agent.radius + obs.radius + epsilon);
+      end
+      distFromStart= sqrt( (obs.coords.x - env.start.coords.x)^2 + (obs.coords.y - env.start.coords.y)^2);
+      if distFromStart <= (env.agent.radius + obs.influenceRange + obs.radius)
+        obs.influenceRange= distFromStart- (env.agent.radius + obs.radius + epsilon);
+      end
+
+      if obs.influenceRange < 0
+        obs.influenceRange = epsilon;
+      end
+      self.obstacles= [ self.obstacles ; obs];
+    end
+
+
+    function generateRandomObs(self,env,number)
+      obsInsertedNum= size(self.obstacles,1);
+      iterNum= 0;
+      while obsInsertedNum < number && iterNum < number*10
+        coords= createRandCoord(self,env);
+        radius = createRandRadius(self,env);
+        collision = collisionCheck(self,coords,radius, obsInsertedNum, env);
+        if ~collision
+          insertObstacle(self, coords, radius, env);
+          obsInsertedNum = obsInsertedNum +1;
+        end
+        iterNum= iterNum + 1;
+      end
+    end
+
+
+    function generateObsFromMat(self, env, mat )
+
+      obsInsertedNum= size(self.obstacles,1);
+      for i= 1:size(mat,1)
+
+        coords.x=  mat(i,1);
+        coords.y=  mat(i,2);
+        coords.z= 0;
+        radius= mat(i,3);
+        collision = collisionCheck(self,coords,radius, obsInsertedNum, env);
+        if ~collision
+          insertObstacle(self, coords, radius, env);
+          obsInsertedNum = obsInsertedNum +1;
+        end
+      end
+    end
+
+    function setObstacles( self, obstacles)
+      self.obstacles = obstacles;
+    end
+
+
+    function collision = collisionCheck(self,coords,radius,currObsNum, env)
+      collision = 0;
+
+
+      range = radius + env.unitaryDim/10;
+      if coords.x <= range || coords.x >= env.length(1,1) - range|| coords.y <= range|| coords.y >= env.length(2,1) -range
+        collision = 1;
+      end
+
+      if currObsNum > 0
+        for i=1:currObsNum
+          obstacle= self.obstacles(i,1);
+          distFromObs= sqrt((coords.x - obstacle.coords.x)^2 +(coords.y - obstacle.coords.y)^2);
+          if  distFromObs - ( radius + obstacle.radius) <= 0
+            collision = 1;
+          end
+        end
+      end
+
+      distFromAgent = sqrt((coords.x - env.agent.coords.x)^2 +(coords.y - env.agent.coords.y)^2);
+      distFromGoal  = sqrt((coords.x - env.goal.coords.x)^2  +(coords.y - env.goal.coords.y )^2);
+      if distFromAgent - ( radius + env.agent.radius) <= 0 || distFromGoal - ( radius + env.agent.radius) <= 0
+        collision= 1;
+      end
+    end
+  end
+end
