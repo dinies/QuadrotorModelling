e296df0c71ef9476821c3f123b99b6791f49e2e6
dinies <edoardo.ghini@live.it>
2018-07-10 13:27:00 +0200

    TELEOP   wrote a part of the second half of the report and created the image grids, missing conclusion and last paragraph of simulations

--- TELEOPERATION/reportTeleop/auto/edoardo.el
+++ TELEOPERATION/reportTeleop/auto/edoardo.el
@@ -3,6 +3,21 @@
  (lambda ()
    (LaTeX-add-labels
     "simParams"
-    "fig:bodo"))
+    "fig:bodo"
+    "fig:10Htz"
+    "fig:100Htz"
+    "fig:1000Htz"
+    "fig:freeRigTot50HR"
+    "fig:freeRigPar50HR"
+    "fig:freeSetTot50HR"
+    "fig:freeSetPar50HR"
+    "fig:freeRigTot20HR"
+    "fig:freeRigPar20HR"
+    "fig:freeSetTot20HR"
+    "fig:freeSetPar20HR"
+    "fig:ContactSetPos"
+    "fig:ContactSetTor"
+    "fig:ContactRigPos"
+    "fig:ContactRigTor"))
  :latex)
 
